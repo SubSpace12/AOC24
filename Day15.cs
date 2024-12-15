2e61591bfdb83709f12b4811df57279f83d4f404
SubSpace <bpawelczyk2005@gmail.com>
2024-12-15 07:15:07 +0100

    Solved day 15 part 1

--- Day15.cs
+++ Day15.cs
@@ -4,27 +4,157 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Security.Permissions;
 
 namespace AdventOfCode.Days
 {
 	internal class Day15
 	{
-		static public string[] input = File.ReadAllLines(@"..\..\Inputs\Day01.txt");
+		static public string[] input = File.ReadAllLines(@"..\..\Inputs\Day15.txt");
 
 
 
-		static public object ParseInput(string[] input)
+		static public (List<List<char>>, List<char>, (int, int)) ParseInput(string[] input)
 		{
 
+			List<List<char>> board = new List<List<char>>();
+			List<char> move= new List<char>();
+			int x = 0;
+			int y = 0;
 
-			return "parsed input";
+			string inputs = String.Join("\n", input);
+			string[] split = inputs.Split(new string[] { "\n\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+			string[] boardString = split[0].Split('\n');
+			for (int i = 0; i < boardString.Length; i++)
+			{	
+				List<char> row = new List<char>();
+				for (int j = 0; j < boardString[i].Length; j++)
+				{
+					row.Add(boardString[i][j]);
+					if (boardString[i][j] == '@')
+					{
+						x = j;
+						y = i;
+					}
+				}
+				board.Add(row);
+			}
+			string[] moveString = split[1].Split('\n');
+			foreach (string line in moveString) 
+			{
+				foreach(char order in line)
+				{
+					move.Add(order);
+				}
+			}
+			return (board, move, (x, y));
 		}
 
 		public object SolvePartOne(string[] input)
 		{
+			long result = 0;
+			(List<List<char>> board, List<char> moves, (int x, int y)) = ParseInput(input);
+			foreach (char move in moves) {
+				if (move == '^') {
+					for (int i = y-1; i >= 0; i--)
+					{
+						if (board[i][x] == '#')
+						{
+							break;
+						}
+						if (board[i][x] == '.')
+						{
+							while (i != y)
+							{
+								board[i][x] = board[i + 1][x];
+								i++;
+							}
+							board[y][x] = '.';
+							y--;
+							break;
+						}
+					}
+				} else if (move == '>')
+				{
+					for (int i = x + 1; i < board[0].Count; i++)
+					{
+						if (board[y][i] == '#')
+						{
+							break;
+						}
+						if (board[y][i] == '.')
+						{
+							while (i != x)
+							{
+								board[y][i] = board[y][i-1];
+								i--;
+							}
+							board[y][x] = '.';
+							x++;
+							break;
+						}
+					}
+					
+				}
+				else if (move == '<')
+				{
+					for (int i = x - 1; i >= 0; i--)
+					{
+						if (board[y][i] == '#')
+						{
+							break;
+						}
+						if (board[y][i] == '.')
+						{
+							while (i != x)
+							{
+								board[y][i] = board[y][i+1];
+								i++;
+							}
+							board[y][x] = '.';
+							x--;
+							break;
+						}
+					}
+				} else if (move == 'v')
+				{
+					for (int i = y + 1; i < board.Count; i++)
+					{
+						if (board[i][x] == '#')
+						{
+							break;
+						}
+						if (board[i][x] == '.')
+						{
+							while (i != y)
+							{
+								board[i][x] = board[i - 1][x];
+								i--;
+							}
+							board[y][x] = '.';
+							y++;
+							break;
+						}
+					}
+				}
+				
+			}
+			for (int i = 0; i < board.Count; i++)
+			{
+				for (int j = 0; j < board[i].Count; j++)
+				{
+					if (board[i][j] == 'O')
+					{
+						result += (100 * i) + j;
+					}
+				}
+			}
 
-			return "part 1 answer";
+			return result;
 		}
+			
+		
 
 		public object SolvePartTwo(string[] input)
 		{
